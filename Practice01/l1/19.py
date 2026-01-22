9d42ba6a4bee87bf4d2ed9780b38d515eefa1162
AdilkhanDaulet <da_adilkhan@kbtu.kz>
2026-01-22 09:00:55 +0500

    lab1

---
+++ Practice01/l1/19.py
@@ -0,0 +1,7 @@
+a=str(input())
+b=str(input())
+c=str(input())
+if b in a:
+    print(a.replace(b,c))
+else:
+    print(a)
---
+++ Practice01/l1/3.py
@@ -0,0 +1,5 @@
+a=int(input())
+if a.isdigit():
+    print("int")
+else:
+    print("str")
