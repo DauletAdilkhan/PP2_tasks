76b60d3d74450d7e9975801cef957637e235eeb9
AdilkhanDaulet <da_adilkhan@kbtu.kz>
2026-01-29 08:33:47 +0500

    w3

--- Practice01/W3/Python_Strings/Escape_char/1ex.py
+++ Practice01/W3/Python_Strings/Escape_char/1ex.py
@@ -1,1 +1,13 @@
 txt = "We are the so-called \"Vikings\" from the north."
+"""
+Code	Result	Try it
+\'	Single Quote	
+\\	Backslash	
+\n	New Line	
+\r	Carriage Return	
+\t	Tab	
+\b	Backspace	
+\f	Form Feed	
+\ooo	Octal value	
+\xhh	Hex value	
+"""
