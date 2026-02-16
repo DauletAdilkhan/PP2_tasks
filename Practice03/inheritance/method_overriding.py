e14c473c297aecb168642d43c34d6d41d7720234
AdilkhanDaulet <da_adilkhan@kbtu.kz>
2026-02-16 20:36:25 +0500

    w3 func,classes,__init__ and  etc

--- Practice03/inheritance/method_overriding.py
+++ Practice03/inheritance/method_overriding.py
@@ -1,4 +1,16 @@
 """--------------------------Add the __init__() Function-------------------------------"""
+class Person:
+  def __init__(self, fname, lname):
+    self.firstname = fname
+    self.lastname = lname
+
+  def printname(self):
+    print(self.firstname, self.lastname)
+
+#Use the Person class to create an object, and then execute the printname method:
+
+x = Person("John", "Doe")
+x.printname()
 #Add the __init__() function to the Student class:
 class Student(Person):
   def __init__(self, fname, lname):
@@ -30,4 +42,3 @@ class Student(Person):
 
   def welcome(self):
     print("Welcome", self.firstname, self.lastname, "to the class of", self.graduationyear)
-
